b2a519877c26e38b3ca0e9f59131809c8443e117
kamini764 <kaminisharma9058@gmail.com>
2024-07-17 11:21:30 +0530

    changes update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/header/Header';
 import Dashboard from './components/dashboard/Dashboard';
-import Pagenotfound from './components/pagenotfound/Pagenotfound';
 
 function App() {
   return (
