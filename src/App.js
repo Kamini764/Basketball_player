7dfd448fed4183ab3613c0ae6453ab76c1e133bf
kamini764 <kaminisharma9058@gmail.com>
2024-07-17 11:10:39 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard/>}/>
-        <Route path="*" element={<Pagenotfound/>}/>
       </Routes>
     </BrowserRouter>
   );
